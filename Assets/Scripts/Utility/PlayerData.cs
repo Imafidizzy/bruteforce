f9e56dd21328c2138865153135230fb07f96dd9e
Edward <edwardchang2000@gmail.com>
2023-04-04 21:09:11 -0400

    Save levels game state

--- Assets/Scripts/Utility/PlayerData.cs
+++ Assets/Scripts/Utility/PlayerData.cs
@@ -9,21 +9,29 @@ namespace Utility
     {
         private string path = "./Assets/Resources/tmp/PlayerData.json";
         private Dictionary<string, Tool> tools = new Dictionary<String, Tool>();
+        private Dictionary<int, Level> levels = new Dictionary<int, Level>();
 
         public void Load()
         {
             string rawData = Utility.IO.ReadString(path);
-            Tool[] data = Utility.JSON.FromJson<Tool>(rawData);
-            foreach(Tool tool in data)
+            PlayerDataModel data = JsonUtility.FromJson<PlayerDataModel>(rawData);
+            foreach(Tool tool in data.Tools)
             {
                 tools.Add(tool.Name, tool);
+            }
+            foreach(Level level in data.Levels)
+            {
+                levels.Add(level.Id, level);
             }
         }
 
         public void Save()
         {
-            List<Tool> data = tools.Values.ToList();
-            string rawData = Utility.JSON.ToJson<Tool>(data.ToArray(), true);
+            PlayerDataModel data = new PlayerDataModel(
+                tools.Values.ToArray(),
+                levels.Values.ToArray()
+            );
+            string rawData = JsonUtility.ToJson(data, true);
             Utility.IO.WriteString(path, rawData);
         }
 
@@ -37,13 +45,43 @@ namespace Utility
             tools[name] = data;
             Save();
         }
+
+        public Level GetLevel(int id)
+        {
+            return levels[id];
+        }
+
+        public void SetLevel(int id, Level data)
+        {
+            levels[id] = data;
+            Save();
+        }
     }
 
     [Serializable]
-    public class Tool
+    public struct Tool
     {
         public string Name;
         public bool Unlocked;
         public int ChargeCount;
+    }
+
+    [Serializable]
+    public struct Level
+    {
+        public int Id;
+        public bool Completed;
+    }
+
+    [Serializable]
+    public struct PlayerDataModel
+    {
+        public Tool[] Tools;
+        public Level[] Levels;
+
+        public PlayerDataModel(Tool[] tools, Level[] levels) {
+            this.Tools = tools;
+            this.Levels = levels;
+        }
     }
 }
