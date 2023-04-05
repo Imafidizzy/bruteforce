349b7f21075ae41a7e60e83da1f6e4f7649e16eb
eustace.imf <47483044+Imafidizzy@users.noreply.github.com>
2023-04-04 23:36:21 -0400

    Final touches

---
+++ Assets/Readme.txt
@@ -0,0 +1,11 @@
+Hello, Our game can be installed, executed, and played simply by downwloading the folders and runnning the executable.
+
+The instructions for hwo to play are included in the game as instruction pages and infographics. Please look through these before beginning any mission.
+
+Our game involved classical ciphers in a story mode fashion. Fllow through the game and use the two available hacker tools to solve the puzzles.
+
+GROUP MEMBER NAMES 
+
+- EDWARD CHANG
+- EUSTACE ONOKHUA IMAFIDON
+- KASHAWN BROWN
--- Assets/Scripts/MissionSelect.cs
+++ Assets/Scripts/MissionSelect.cs
@@ -16,8 +16,8 @@ public class MissionSelect : MonoBehaviour
     public GameObject frontArrow;
     public TMP_Text displayText;
     public Image displayed;
-    string caesarDesc = "Caesar";
-    string vigenereDesc = "Vigenere";
+    string caesarDesc = "The caesar tool requires you to enter the encypted text in the 'code' section and a number in the 'key' section. Caesar encryoption moves all alphabets forward/backwards by the key when encrypting/decrypting.";
+    string vigenereDesc = "The vigenere tool takes a word or combination of letters as its key. It works like the caersar cipher but each letter of the text being encrypted is shifted by a different amount. These amounts follow the pattern of the letter in the key. It will loop around the letters in the key until all letters in the code have been encrypted";
     public Sprite caesar;
     public Sprite vigenere;
     private int maxUnlocked;
